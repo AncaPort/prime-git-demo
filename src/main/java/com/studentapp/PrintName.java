c50cbdc5073ff8be040f142498a7b264355bef3a
shitalsanghani <sanghanishital00@gmail.com>
2024-07-20 15:29:37 +0100

    change name in two lines

--- src/main/java/com/studentapp/PrintName.java
+++ src/main/java/com/studentapp/PrintName.java
@@ -3,5 +3,6 @@ package com.studentapp;
 public class PrintName {
     public static void main(String[] args) {
         System.out.println("Shital Sanghani");
+        System.out.println("Tester");
     }
 }
